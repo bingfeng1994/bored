0e0674976b1e1f62b4ba07203ac7f846efbbb44e
wanghao <359446646@qq.com>
2018-09-06 09:16:38 +0800

    "9/06"

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import axios from 'axios'
+import axios from 'axios'//引入axios
+import ElementUI from 'element-ui';//引入饿了么UI
+import 'element-ui/lib/theme-chalk/index.css';//引入饿了么UI样式表
 
 Vue.prototype.$ajax = axios   //axios不是vue的插件,不可以用use
 Vue.config.productionTip = false
+Vue.use(ElementUI);
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+const HelloWorld = () => import('@/components/HelloWorld')
+const Login = () => import('@/components/login')//登陆
+const Home = () => import('@/components/home')//首页
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+//  {
+//  	path: '/',
+//			redirect: '/home'
+//  },
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/login',
+      component: Login
+    },
+    {
+    	path: '/home',
+			component: Home
     }
   ]
 })
